c996a64a586d26a4b2b6a0a8c87995e694670901
Cahlan Sharp <cahlan@gmail.com>
2022-02-11 16:51:53 -0700

    Update create-droplet.sh

--- create-droplet.sh
+++ create-droplet.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
 
+KEYS=$(curl -X GET "https://api.digitalocean.com/v2/account/keys" \
+-H "Authorization: Bearer $DIGITAL_OCEAN_TOKEN" | jq '.ssh_keys | .[].id|tostring' | awk 'NR > 1 { printf(",") } {printf "%s",$0}')
+echo "ssh_keys:[$KEYS]"
+USER_DATA="#!/bin/bash
 
-curl -X GET "https://api.digitalocean.com/v2/account/keys" \
--H "Authorization: Bearer $DIGITAL_OCEAN_TOKEN" | jq '.ssh_keys | map(.id) | join(",")'
+echo saraza > ~/file.txt"
+  -H "Content-Type: application/json"
+DATA="{\"name\":\"example.com\",\"region\":\"sfo3\",\"size\":\"s-1vcpu-1gb\",\"image\":\"ubuntu-20-04-x64\",\"ssh_keys\":[$KEYS],\"backups\":false,\"ipv6\":false,\"user-data\":\"$USER_DATA\"}"
+echo $DATA
+curl -X POST "https://api.digitalocean.com/v2/droplets" \
+  -H "Authorization: Bearer $DIGITAL_OCEAN_TOKEN" \
+  -d "$DATA"
